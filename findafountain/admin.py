5c9e0d887f2101c0bd14392b68294e42901bb692
Giedre Stulgyte <2285161s@student.gla.ac.uk>
2018-03-04 15:34:35 +0000

    delete all

--- findafountain/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from findafountain.models import UserProfile, Fountain
-
-class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug':('id',)}
-
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Fountain)
